b69eaceaa0d4f3febcb7e93592e9d385a4fb271d
mckay <mckaybwarren@gmail.com>
2022-12-14 15:19:22 -0500

    refactored book id validation

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 #file will define endpoints
 
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, abort, make_response
 
 class Book:
     def __init__(self, id=None, title=None, description=None):
@@ -29,15 +29,21 @@ def handle_books():
 
 @books_bp.route("/<book_id>", methods=["GET"])
 def handle_book(book_id):
+    book = validate_book(book_id)
+    return {
+        "id": book.id,
+        "title": book.title,
+        "description": book.description
+    }, 200
+
+def validate_book(book_id):
     try:
         book_id = int(book_id)
     except ValueError:
-        return {"message": f"book id {book_id} invalid"}, 400
+        abort(make_response({"message": f"book id {book_id} invalid"}, 400))
+
     for book in books:
-        if book.id == book_id:
-            return {
-                "id": book.id,
-                "title": book.title,
-                "description": book.description
-            }, 200
-        return {"message": f"book {book_id} not found"}, 404
+        if book.id == book_id: 
+            return book
+
+    abort(make_response({"message":f"book {book_id} not found"}, 404))
