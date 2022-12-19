c2e4da1d9330b6e739783efe17dc06d7f36a2b5a
mckay <mckaybwarren@gmail.com>
2022-12-19 07:44:42 -0600

    add endpoint for reading all books

--- app/routes.py
+++ app/routes.py
@@ -4,12 +4,26 @@ from flask import Blueprint, jsonify, make_response, request
 
 books_bp = Blueprint("books", __name__, url_prefix="/books")
 
-@books_bp.route("", methods=["POST"])
+@books_bp.route("", methods=["GET", "POST"])
 def handle_books():
-    request_body = request.get_json()
-    new_book = Book(title=request_body["title"], description=request_body["description"])
+    if request.method == "GET":
+        books = Book.query.all()
+        books_response = []
 
-    db.session.add(new_book)
-    db.session.commit()
+        for book in books:
+            books_response.append({
+                "id": book.id,
+                "title": book.title,
+                "description": book.description
+            })
 
-    return make_response(f"Book {new_book.title} successfully created", 201)
+        return jsonify(books_response )
+
+    elif request.method == "POST":
+        request_body = request.get_json()
+        new_book = Book(title=request_body["title"], description=request_body["description"])
+
+        db.session.add(new_book)
+        db.session.commit()
+        
+        return make_response(f"Book {new_book.title} successfully created", 201)
