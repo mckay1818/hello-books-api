681d7fff2424abee462007d43b6cd7d3f12c2f1e
mckay <mckaybwarren@gmail.com>
2023-01-03 10:53:21 -0500

    create Book method for conversion to dict

--- app/models/book.py
+++ app/models/book.py
@@ -4,3 +4,11 @@ class Book(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String)
     description = db.Column(db.String)
+
+    def to_dict(self):
+        dict = {}
+        dict["id"] = self.id
+        dict["title"] = self.title
+        dict["description"] = self.description
+
+        return dict
