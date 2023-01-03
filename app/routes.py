61be95a717812879a4bf052024407fe8d10214c0
mckay <mckaybwarren@gmail.com>
2023-01-03 10:56:05 -0500

    implemented to_dict method in routes

--- app/routes.py
+++ app/routes.py
@@ -24,12 +24,7 @@ def read_all_books():
 
     books_response = []
     for book in books:
-        books_response.append({
-            "id": book.id,
-            "title": book.title,
-            "description": book.description
-        })
-
+        books_response.append(book.to_dict())
     return jsonify(books_response)
 
 def validate_book(book_id):
@@ -48,11 +43,7 @@ def validate_book(book_id):
 @books_bp.route("/<book_id>", methods=["GET"])
 def read_one_book(book_id):
     book = validate_book(book_id)
-    return {
-        "id": book.id,
-        "title": book.title,
-        "description": book.description
-    }
+    return book.to_dict()
 
 @books_bp.route("/<book_id>", methods=["PUT"])
 def update_book(book_id):
