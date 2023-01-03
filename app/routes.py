c242b87e69da61a133eb1109189a6830d7e3a757
mckay <mckaybwarren@gmail.com>
2023-01-02 20:28:30 -0500

    add tests for POST request

--- app/routes.py
+++ app/routes.py
@@ -12,7 +12,7 @@ def create_book():
     db.session.add(new_book)
     db.session.commit()
 
-    return make_response(f"Book {new_book.title} successfully created", 201)
+    return make_response(jsonify(f"Book {new_book.title} successfully created"), 201)
 
 @books_bp.route("", methods=["GET"])
 def read_all_books():
@@ -36,12 +36,12 @@ def validate_book(book_id):
     try:
         book_id = int(book_id)
     except:
-        abort(make_response({"message": f"book {book_id} invalid"}, 400))
+        abort(make_response(jsonify({"message": f"book {book_id} invalid"}), 400))
 
     book = Book.query.get(book_id)
 
     if not book:
-        abort(make_response({"message": f"book {book_id} not found"}, 404))
+        abort(make_response(jsonify({"message": f"book {book_id} not found"}), 404))
 
     return book
 
@@ -63,7 +63,7 @@ def update_book(book_id):
     book.description = request_body["description"]
 
     db.session.commit()
-    return make_response({"message": f"Book #{book.id} successfully updated"}, 200)
+    return make_response(jsonify({"message": f"Book #{book.id} successfully updated"}), 200)
 
 @books_bp.route("/<book_id>", methods=["DELETE"])
 def delete_book(book_id):
@@ -72,4 +72,4 @@ def delete_book(book_id):
     db.session.delete(book)
     db.session.commit()
 
-    return make_response({"message": f"Book #{book.id} successfully deleted"}, 200)
+    return make_response(jsonify({"message": f"Book #{book.id} successfully deleted"}), 200)
