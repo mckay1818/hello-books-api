0caffb853dffb0c7d713fe6f497cbc27f4ed01a9
mckay <mckaybwarren@gmail.com>
2023-01-04 12:15:19 -0500

    create Author model with one-to-many relationship with Book model

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from app import db
 from app.models.book import Book
+from app.models.author import Author
 from flask import Blueprint, jsonify, abort, make_response, request
 
 books_bp = Blueprint("books", __name__, url_prefix="/books")
@@ -65,3 +66,26 @@ def delete_book(book_id):
     db.session.commit()
 
     return make_response(jsonify(f"Book #{book.id} successfully deleted"), 200)
+
+@authors_bp.route("", methods=["POST"])
+def add_one_authors():
+    request_body = request.get_json()
+    new_author = Author.from_dict(request_body)
+
+    db.session.add(new_author)
+    db.session.commit()
+
+    return make_response(jsonify(f"Author {new_author.name} successfully created"), 201)
+
+@authors_bp.route("", methods=["GET"])
+def read_all_authors():
+    name_query = request.args.get("name")
+    if name_query:
+        authors = Author.query.filter_by(name=name_query)
+    else:
+        authors = Author.query.all()
+
+    authors_response = []
+    for author in authors:
+        authors_response.append(author.to_dict())
+    return jsonify(authors_response)
